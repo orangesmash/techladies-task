cb9564789be0a29a79ed829f205700d9b2fa323b
AngelaE <angela.espehana@gmail.com>
2017-07-01 16:24:47 +0000

    Edit and delete controllers and views added. Notice messages added.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,13 +8,43 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def new
+        @article = Article.new
+    end 
     
     def create
         @article = Article.new(article_params)
-        @article.save
         
-        redirect_to article_path(@article)
+        if @article.save
+            message = { notice: "Article '#{@article.title}' saved!" }
+        else 
+            message = {error: @article.errors.full_messages.to_sentence }
+        end
+        
+        redirect_to article_path(@article), flash: message
         
     end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
 
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+        
+        flash.notice = "Article '#{@article.title}' Updated!"
+        
+        redirect_to article_path(@article)
+    end
+    
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        
+        flash.notice = "Article '#{@article.title}' Deleted!"
+        
+        redirect_to articles_path
+        
+    end
 end
