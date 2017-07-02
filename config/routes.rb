243a64b2e2458050b9363f60e1142703443c47ad
AngelaE <angela.espehana@gmail.com>
2017-07-02 17:35:08 +0000

    Updated layout of home page and overall website

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-    root to: 'articles#index'
+namespace :articles do
+  root to: 'articles#index'
   resources :articles
+end 
+
+#Routes for static pages
+namespace :pages do
+    get "/pages/:page" => "pages#show"
+    root "pages#show", page: "home" 
+end
+
 end
